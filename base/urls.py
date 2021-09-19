01843fe1feff1deb560d04c291ab21bd80d8fe2f
Harry Khanna <harry@khanna.cc>
2021-09-19 00:11:18 -0400

    Remove unused allauth views

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,27 @@
 import django.views.defaults
+from django.http import Http404
 from django.urls import path, include
 from . import views
+
+page_not_found = lambda request: django.views.defaults.page_not_found(request, None)
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("accounts/settings/", views.SettingsView.as_view(), name="account_settings"),
     path("accounts/delete/", views.DeleteView.as_view(), name="account_delete"),
+    path("accounts/password/change/", page_not_found, name="account_change_password"),
+    path(
+        "accounts/password/set/",
+        page_not_found,
+        name="account_set_password",
+    ),
+    path("accounts/inactive/", page_not_found, name="account_inactive"),
+    path("accounts/email/", page_not_found, name="account_email"),
+    path(
+        "accounts/confirm-email/",
+        page_not_found,
+        name="account_email_verification_sent",
+    ),
     path("accounts/", include("allauth.urls")),
     path(
         "render-template-debug/<path:template>",
@@ -16,6 +32,6 @@ urlpatterns = [
     path(
         "403/", lambda request: django.views.defaults.permission_denied(request, None)
     ),
-    path("404/", lambda request: django.views.defaults.page_not_found(request, None)),
+    path("404/", page_not_found),
     path("500/", django.views.defaults.server_error),
 ]
