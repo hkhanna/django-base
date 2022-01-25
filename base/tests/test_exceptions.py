b753baec5adf3b8e10afb13ba4006e9996c5cd45
Harry Khanna <harry@khanna.cc>
2022-01-24 19:26:45 -0500

    PermissionDenied test should not use IndexView

--- base/tests/test_exceptions.py
+++ base/tests/test_exceptions.py
@@ -1,30 +1,32 @@
 from unittest.mock import Mock
 from django.urls import reverse
 from django.core.exceptions import PermissionDenied
-from ..views import IndexView
+from ..views import SettingsView
 from .assertions import assertMessageContains
 
 
-def test_permission_denied_default(client, settings, monkeypatch):
+def test_permission_denied_default(client, user, settings, monkeypatch):
     """PermissionDenied exception redirects by default to PERMISSION_DENIED_REDIRECT"""
     message = "You are a a nice person, but you don't have access to this."
     monkeypatch.setattr(
-        IndexView, "get_context_data", Mock(side_effect=PermissionDenied(message))
+        SettingsView, "get", Mock(side_effect=PermissionDenied(message))
     )
-    response = client.get(reverse("index"))
+    client.force_login(user)
+    response = client.get(reverse("account_settings"))
     assert response.status_code == 302
     assert response.url == settings.PERMISSION_DENIED_REDIRECT
     assertMessageContains(response, message)
 
 
-def test_permission_denied_redirect(client, monkeypatch):
+def test_permission_denied_redirect(client, user, monkeypatch):
     """PermissionDenied exception can take an optional url to redirect to"""
     message = "You are a a nice person, but you don't have access to this."
     url = reverse("terms_of_use")
     monkeypatch.setattr(
-        IndexView, "get_context_data", Mock(side_effect=PermissionDenied(message, url))
+        SettingsView, "get", Mock(side_effect=PermissionDenied(message, url))
     )
-    response = client.get(reverse("index"))
+    client.force_login(user)
+    response = client.get(reverse("account_settings"))
     assert response.status_code == 302
     assert response.url == url
     assertMessageContains(response, message)
