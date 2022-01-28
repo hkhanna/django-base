448a6e3e13c99e2ec068dc89d281d349e6b9975d
Harry Khanna <harry@khanna.cc>
2022-01-28 14:27:17 -0500

    Wrap entire logrocket template tag in a try/catch

--- base/templatetags/logrocket.py
+++ base/templatetags/logrocket.py
@@ -7,24 +7,24 @@ register = template.Library()
 
 @register.inclusion_tag("base/logrocket.html", takes_context=True)
 def logrocket(context):
-    # If there's a problem getting the current URL, don't bother.
-    # This can happen, e.g., in a 404 situation.
+    # If there's a problem with any of this, don't bother.
+    # This can happen, e.g., in a 404 situation where some info
+    # is missing on the request.
     try:
         request = context["request"]
         current_view = resolve(request.path_info).view_name
+        if current_view in settings.LOGROCKET_EXCLUDED_VIEWS:
+            return None
+
+        user = request.user
+        if user.is_authenticated:
+            return {
+                "logrocket_id": settings.LOGROCKET_APP_ID,
+                "user_id": user.pk,
+                "user_name": user.name,
+                "user_email": user.email,
+            }
+        else:
+            return {"logrocket_id": settings.LOGROCKET_APP_ID}
     except Exception:
         return None
-
-    if current_view in settings.LOGROCKET_EXCLUDED_VIEWS:
-        return None
-
-    user = request.user
-    if user.is_authenticated:
-        return {
-            "logrocket_id": settings.LOGROCKET_APP_ID,
-            "user_id": user.pk,
-            "user_name": user.name,
-            "user_email": user.email,
-        }
-    else:
-        return {"logrocket_id": settings.LOGROCKET_APP_ID}
