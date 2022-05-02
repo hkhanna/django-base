b9695affb11bf5109b08a38943bdfb5b5858ee89
Harry Khanna <harry@khanna.cc>
2022-05-02 12:18:38 -0400

    Minimize what's in main.js

--- base/frontend/src/js/main.js
+++ base/frontend/src/js/main.js
@@ -1,30 +1,12 @@
 import '../css/styles.css'
-// This is the entry point for any JS you want to include.
-// This file should be loaded after any vite assets that are needed by Alpine.
-
-// AlpineJS
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
+
+// This is the entry point for any JS you want to include.
+// This file should be loaded after any vite assets that are needed by Alpine.
+// Add Javascript here.
+
+// AlpineJS
 Alpine.plugin(focus)
 window.Alpine = Alpine
 Alpine.start()
-
-// Utility functions
-// Add a Django Form to a Formset
-window.addForm = (selector, totalFormSelector) => {
-  let totalFormEl = document.querySelector(totalFormSelector)
-  let templateEl = document.querySelector(selector)
-
-  // Clone the node
-  let cloned = templateEl.content.cloneNode(true)
-
-  // Replace __prefix__ with the incremented total form count
-  for(let child of cloned.querySelectorAll("label,input")) {
-    child.outerHTML = child.outerHTML.replaceAll("__prefix__", totalFormEl.value)
-  }
-  // Insert the updated node
-  templateEl.after(cloned)
-
-  // Increment the total form count
-  totalFormEl.value++
-}
