69a99c2ad555a3a05c65096fed945edfbcb2d327
Harry Khanna <harry@khanna.cc>
2022-10-25 12:45:31 -0400

    htmx as a frontend dependency

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,13 +1,14 @@
-import '../css/styles.css'
-import '../css/fonts.css'
-import Alpine from 'alpinejs'
-import focus from '@alpinejs/focus'
+import "../css/styles.css";
+import "../css/fonts.css";
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
+import "htmx.org";
 
 // This is the entry point for any JS you want to include.
 // This file should be loaded after any vite assets that are needed by Alpine.
 // Add Javascript here.
 
 // AlpineJS
-Alpine.plugin(focus)
-window.Alpine = Alpine
-Alpine.start()
+Alpine.plugin(focus);
+window.Alpine = Alpine;
+Alpine.start();
