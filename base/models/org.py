89a452e5f704c99f80705251dc4fb182b5b4ecb7
Harry Khanna <harry@khanna.cc>
2023-03-16 15:11:41 -0400

    Scaffold Org/OrgUser permission models

--- base/models/org.py
+++ base/models/org.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 class Org(models.Model):
     """Organizations users can belong to. They must belong to at least one."""
 
+    name = models.CharField(max_length=254, help_text="The name of the organization")
     slug = AutoSlugField(
         max_length=127,
         blank=True,
@@ -20,7 +21,6 @@ class Org(models.Model):
         unique=True,
         help_text="The name in all lowercase, suitable for URL identification",
     )
-    name = models.CharField(max_length=254, help_text="The name of the organization")
     is_active = models.BooleanField(default=True)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="owned_orgs"
@@ -71,6 +71,8 @@ class Org(models.Model):
 
 
 class OrgUser(models.Model):
+    """The 'membership' model for a User/Org relationship."""
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name="org_users", on_delete=models.CASCADE
     )
@@ -87,3 +89,143 @@ class OrgUser(models.Model):
         constraints = [
             models.UniqueConstraint(fields=["user", "org"], name="unique_user_org")
         ]
+
+
+class Plan(models.Model):
+    """Represents a group of OrgSettings. Often tied to billing."""
+
+    name = models.CharField(max_length=254, unique=True)
+    slug = AutoSlugField(
+        max_length=127,
+        blank=True,
+        overwrite=True,
+        populate_from="name",
+        unique=True,
+        help_text="The name in all lowercase, suitable for URL identification",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrgSetting(models.Model):
+    """An Org-wide setting"""
+
+    slug = models.SlugField(max_length=254, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    default = models.IntegerField(default=0)
+
+
+class PlanOrgSetting(models.Model):
+    """The Org-wide settings for a particular Plan. Billing-related limitations go here."""
+
+    plan = models.ForeignKey(
+        "base.Plan", on_delete=models.CASCADE, related_name="plan_org_settings"
+    )
+    setting = models.ForeignKey(
+        "base.OrgSetting", on_delete=models.CASCADE, related_name="plan_org_settings"
+    )
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["plan", "setting"], name="unique_plan_setting"
+            )
+        ]
+
+    def __str__(self):
+        return self.setting.slug
+
+
+class OverriddenOrgSetting(models.Model):
+    """Used if a particular Org should override the Plan Org-wide settings."""
+
+    # Useful if there's a custom plan for a customer or if there's only one Plan but customers
+    # can make one-time purchases potentially unlocking certain settings.
+    org = models.ForeignKey(
+        "base.Org", on_delete=models.CASCADE, related_name="overridden_org_settings"
+    )
+    setting = models.ForeignKey(
+        "base.OrgSetting",
+        on_delete=models.CASCADE,
+        related_name="overridden_org_settings",
+    )
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["org", "setting"], name="unique_overridden_org_setting"
+            )
+        ]
+
+    def __str__(self):
+        return self.setting.slug
+
+
+class OrgUserSetting(models.Model):
+    """Settings for members of an Org"""
+
+    slug = models.SlugField(max_length=254, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    default = models.IntegerField(default=0)
+
+
+class OrgUserOrgUserSetting(models.Model):
+    """The specific mapping of an OrgUser to an OrgUserSetting."""
+
+    org_user = models.ForeignKey(
+        "base.OrgUser",
+        on_delete=models.CASCADE,
+        related_name="org_user_org_user_settings",
+    )
+    setting = models.ForeignKey(
+        "base.OrgUserSetting",
+        on_delete=models.CASCADE,
+        related_name="org_user_org_user_settings",
+    )
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["org_user", "setting"], name="unique_org_user_setting"
+            )
+        ]
+
+    def __str__(self):
+        return self.setting.slug
+
+
+class OrgUserSettingDefaults(models.Model):
+    """An Org can set defaults for its OrgUsers that have not specifically set certain settings."""
+
+    org = models.ForeignKey(
+        "base.Org", on_delete=models.CASCADE, related_name="org_user_setting_defaults"
+    )
+    setting = models.ForeignKey(
+        "base.OrgUserSetting",
+        on_delete=models.CASCADE,
+        related_name="org_user_setting_defaults",
+    )
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["org", "setting"], name="unique_org_user_setting_defaults"
+            )
+        ]
+
+    def __str__(self):
+        return self.setting.slug
---
+++ base/tests/test_org_settings.py
@@ -0,0 +1,33 @@
+"""Tests related to settings for Orgs, OrgUsers, and Plans."""
+
+# The general philosophy is this.
+# There are 2 types of settings, settings for an Org (OrgSettings) and settings for an OrgUser (OrgUserSettings).
+# They are related to Orgs, OrgUsers and Plans in different ways.
+# - OrgSettings are primarily related to a payment Plan. An Org may override an OrgSetting, in which case it will not look to the Plan for that OrgSetting.
+# - If a Plan is queried for an OrgSetting, and that OrgSetting is not set on the Plan, it will materialize the setting on the Plan with the OrgSetting's default.
+# - OrgUserSettings should have defaults set by the Org. If an Org is accessed for an OrgUserSetting default and it's not there, it will materialize it on the Org.
+#    - At this point, it doesn't seem useful to attach OrgUserSetting defaults to a Plan, so we don't. We can easily change this down the road though.
+# - If a setting does not exist but is queried, that setting will autocreate with a value of 0.
+
+# A one-time payment situation would probably only use the default Plan and override OrgSettings as the purchase is made.
+
+
+"""Org.get_setting() in the normal case will retrieve the OrgSetting from the Plan"""
+
+"""Org.get_setting() will prioritize any OrgSettingOverrides"""
+
+"""Org.get_setting() will materialize an OrgSetting on the Plan if it isn't already on the Plan"""
+
+"""Org.get_setting() will create an OrgSetting with a default of 0 if it is accessed but does not exist"""
+
+"""If Org.current_period_end is expired, Org.get_setting() should look to Org.default_plan"""
+
+"""If Org.current_period_end is None, the plan never expires."""
+
+"""OrgUser.get_setting() where the OrgUser is the owner does... what?"""
+
+"""OrgUser.get_setting() will first look to a direct setting on the OrgUser"""
+
+"""OrgUser.get_setting() will look to Org defaults (materializing if necessary) if there is no direct setting on the OrgUser"""
+
+"""OrgUser.get_setting() will create an OrgUserSetting with a default of 0 if it is accessed but does not exist"""
