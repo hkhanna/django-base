3da58f2a7987e73a5a4130042f552c4ff5f04a96
Harry Khanna <harry@khanna.cc>
2023-08-11 16:15:21 -0400

    AlpineJS submit modal modifers

--- frontend/src/js/alpine-components/modal.js
+++ frontend/src/js/alpine-components/modal.js
@@ -1,20 +1,119 @@
+const VARIANTS = {
+  primary: {
+    defaultIcon: "check-circle",
+    iconBgColor: "bg-indigo-100",
+    iconColor: "text-indigo-700",
+  },
+  secondary: {
+    defaultIcon: "information",
+    iconBgColor: "bg-blue-100",
+    iconColor: "text-blue-500",
+  },
+  danger: {
+    defaultIcon: "exclamation-circle",
+    iconBgColor: "bg-red-100",
+    iconColor: "text-red-600",
+  },
+};
+
 export default function (Alpine) {
   Alpine.directive(
     "modal",
     (el, { value, modifiers, expression }, { effect, evaluate }) => {
-      console.log(el.innerHTML.toString().trim());
-      const innerHTML = render({
-        show: expression,
+      const modalDoc = renderModalDoc({
+        show: value,
         body: el.innerHTML.toString().trim(),
       });
 
-      el.innerHTML = innerHTML;
+      if (modifiers.includes("submit")) {
+        const {
+          defaultIcon,
+          iconBgColor,
+          iconColor,
+          formnoaction,
+          formnovalidate,
+        } = processModifiers(modifiers);
+
+        const { icon, title, submitLabel, submitName, submitValue } =
+          evaluate(expression);
+
+        modalDoc.getElementById("modal-body").outerHTML = renderSubmitModal({
+          icon: icon || defaultIcon,
+          iconBgColor,
+          iconColor,
+          title,
+          submitLabel,
+          formnovalidate,
+          formnoaction,
+          submitName,
+          submitValue,
+        });
+      }
+
+      modalDoc.getElementById("modal-body").outerHTML = el.innerHTML;
+
+      el.parentNode.replaceChild(modalDoc.body.firstChild, el);
     }
   );
 }
 
-function render(props) {
-  let templ = `
+function processModifiers(modifiers) {
+  let props = { ...VARIANTS.primary };
+
+  if (modifiers.includes("secondary")) {
+    props = { ...VARIANTS.secondary };
+  }
+  if (modifiers.includes("danger")) {
+    props = { ...VARIANTS.danger };
+  }
+
+  if (modifiers.includes("formnoaction")) {
+    props.formnoaction = true;
+  }
+
+  if (modifiers.includes("formnovalidate")) {
+    props.formnovalidate = true;
+  }
+
+  return props;
+}
+
+function renderSubmitModal({
+  icon,
+  iconBgColor,
+  iconColor,
+  title,
+  submitLabel,
+  formnovalidate,
+  formaction,
+  submitName,
+  submitValue,
+}) {
+  return `
+  <div>
+  <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full ${iconBgColor}">
+    <div x-heroicon:outline=${icon} class="h-6 w-6 ${iconColor}" aria-hidden="true"></div>
+  </div>
+  <div class="mt-3 text-center sm:mt-5">
+    <h3 class="text-lg leading-6 font-medium text-gray-900">
+      ${title}
+    </h3>
+    <div class="mt-2">
+      <p class="text-sm text-gray-800">
+        <div id="modal-body"></div>
+      </p>
+    </div>
+  </div>
+</div>
+  <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+    {% component "button" variant=variant name=name value=value type="submit" size="lg" extra_class="w-full justify-center sm:col-start-2" text=submit_button_text formnovalidate=formnovalidate formaction=formaction %}
+    {% component "button" variant="white" type="button" size="lg" extra_class="mt-3 w-full justify-center sm:mt-0 sm:col-start-1" text="Cancel" click=show|add:" = false" %}
+  </div>
+  `;
+}
+
+function renderModalDoc(props) {
+  const templ = `
     <div 
     x-show="${props.show}"
     x-on:keyup.document.escape="${props.show} = false"
@@ -53,17 +152,13 @@ function render(props) {
               <div x-heroicon:outline="x-mark" class="h-6 w-6" aria-hidden="true"></div>
             </button>
           </div>
-        `;
-
-  if (props.body.length > 0) {
-    templ += `${props.body}`;
-  }
-  templ += `
+          <div id="modal-body"></div>
         </div>
       </div>
     </div>
   </div>
   `;
-
-  return templ;
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(templ, "text/html");
+  return doc;
 }
