c6e32299227309cfaba21fe278076916d0f8c9d9
Harry Khanna <harry@khanna.cc>
2023-08-11 09:44:11 -0400

    Alert alpine component sans icons

--- frontend/src/js/alpine-components/alert.js
+++ frontend/src/js/alpine-components/alert.js
@@ -1,18 +1,98 @@
-export default function (Alpine) {
-  Alpine.directive("alert", (el, { expression }, { effect, evaluate }) => {
-    const headline = expression;
-    el.classList.add(
-      ..."relative w-full rounded-lg border bg-white p-4 [&>svg]:absolute [&>svg]:text-foreground [&>svg]:left-4 [&>svg]:top-4 [&>svg+div]:translate-y-[-3px] [&:has(svg)]:pl-11 text-nuetral-900".split(
-        " "
-      )
-    );
+import CheckCircleIcon from "heroicons/24/solid/check-circle.svg";
+import XMark from "heroicons/20/solid/x-mark.svg";
 
-    const innerHtml = `
-      <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" x2="20" y1="19" y2="19"></line></svg>
-      <h5 class="mb-1 font-medium leading-none tracking-tight">${headline}</h5>
-      <div class="text-sm opacity-70">${el.innerHTML.toString()}</div>
+const COLORS = {
+  success: {
+    bg: "bg-green-100",
+    iconSvg: CheckCircleIcon,
+    icon: "text-green-500",
+    headline: "text-green-800",
+    body: "text-green-700",
+    button:
+      "bg-green-100 text-green-700 hover:bg-green-100 focus:ring-green-600",
+  },
+  warning: {
+    bg: "bg-yellow-50",
+    iconSvg: "exclamation-circle",
+    icon: "text-yellow-400",
+    headline: "text-yellow-800",
+    body: "text-yellow-600",
+    button:
+      "bg-yellow-50 text-yellow-700 hover:bg-yellow-50 focus:ring-yellow-600",
+  },
+  info: {
+    bg: "bg-blue-100",
+    iconSvg: "information-circle",
+    icon: "text-blue-500",
+    headline: "text-blue-700",
+    body: "text-blue-600",
+    button: "bg-blue-100 text-blue-700 hover:bg-blue-100 focus:ring-blue-600",
+  },
+  error: {
+    bg: "bg-red-50",
+    iconSvg: "x-circle",
+    icon: "text-red-400",
+    headline: "text-red-800",
+    body: "text-red-700",
+    button: "bg-red-50 text-red-800 hover:bg-red-50 focus:ring-red-700",
+  },
+};
+
+const render = (props) => {
+  let templ = `
+  <div class="rounded-md ${props.colors.bg} p-4" x-data="{ open: true }" x-show="open">
+    <div class="flex items-start">
+      <div class="flex-shrink-0">
+        <svg class="h-5 w-5 ${props.colors.icon} xmlns="http://www.w3.org/2000/svg">
+          <use xlink:href="${props.colors.iconSvg}"></use>
+        </svg>
+      </div>
+      <div class="ml-3">
+        <h3 class="text-sm font-medium ${props.colors.headling}">
+          ${props.headline}
+        </h3>
+      </div>
     `;
+  if (props.dismissable) {
+    templ += `
+        <div class="ml-auto pl-3">
+          <div class="-mx-1.5 -my-1.5">
+            <button type="button" class="inline-flex rounded-md p-1.5 focus:outline-none focus:ring-2 focus:ring-offset-2 ${props.colors.button}" x-on:click="open = !open">
+              <span class="sr-only">Dismiss</span>
+              <img src=${XMark} class="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        `;
+  }
+  templ += "</div>";
 
-    el.innerHTML = innerHtml;
-  });
+  if (props.body.length > 0) {
+    templ += `
+      <div class="ml-8 mt-2 text-sm ${props.colors.body}">
+        ${props.body}
+      </div>
+      `;
+  }
+
+  templ += "</div>";
+  return templ;
+};
+
+export default function (Alpine) {
+  Alpine.directive(
+    "alert",
+    (el, { value, modifiers, expression }, { effect, evaluate }) => {
+      const headline = expression;
+
+      const colors = COLORS[value] || COLORS.info;
+
+      el.innerHTML = render({
+        colors: colors,
+        headline: headline,
+        body: el.innerHTML.toString().trim(),
+        dismissable: modifiers.includes("dismissable"),
+      });
+    }
+  );
 }
