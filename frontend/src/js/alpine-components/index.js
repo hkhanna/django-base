d5cad1c04634d1bf24a051fab98e27992e7d7ca5
Harry Khanna <harry@khanna.cc>
2023-10-02 17:41:51 -0400

    Datepicker skeleton

--- frontend/src/js/alpine-components/index.js
+++ frontend/src/js/alpine-components/index.js
@@ -2,10 +2,12 @@ import alert from "./alert.js";
 import heroicon from "./heroicon.js";
 import modal from "./modal.js";
 import btnIcon from "./btn-icon.js";
+import datepicker from "./x-datepicker.js";
 
 export default function (Alpine) {
   Alpine.plugin(alert);
   Alpine.plugin(heroicon);
   Alpine.plugin(modal);
   Alpine.plugin(btnIcon);
+  Alpine.plugin(datepicker);
 }
