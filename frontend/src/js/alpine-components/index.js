7ae4268075405f3ff6248cb9d1bc574799eb425d
Harry Khanna <harry@khanna.cc>
2023-10-02 17:42:39 -0400

    Rename alpine component files

--- frontend/src/js/alpine-components/index.js
+++ frontend/src/js/alpine-components/index.js
@@ -1,7 +1,7 @@
-import alert from "./alert.js";
-import heroicon from "./heroicon.js";
-import modal from "./modal.js";
-import btnIcon from "./btn-icon.js";
+import alert from "./x-alert.js";
+import heroicon from "./x-heroicon.js";
+import modal from "./x-modal.js";
+import btnIcon from "./x-btn-icon.js";
 import datepicker from "./x-datepicker.js";
 
 export default function (Alpine) {
