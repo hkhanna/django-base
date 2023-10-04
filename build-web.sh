1f0b8cbef14e5d6267711319a74d07c0db837ca0
Harry Khanna <harry@khanna.cc>
2023-10-04 18:55:49 -0400

    Setup periodic tasks management command

--- build-web.sh
+++ build-web.sh
@@ -11,3 +11,4 @@ npm install --prefix frontend
 npm run build --prefix frontend
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py setup_periodic_tasks
---
+++ core/management/commands/setup_periodic_tasks.py
@@ -0,0 +1,49 @@
+import os
+from django.apps import apps
+import importlib
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django_celery_beat.models import IntervalSchedule, CrontabSchedule, PeriodicTask
+
+
+class Command(BaseCommand):
+    help = f"""Setup celery beat periodic tasks."""
+
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        print("Deleting all periodic tasks and schedules...\n")
+
+        # Grab the periodic variable from the top level of every app's tasks.py.
+        periodic_specs = []
+
+        # Iterate over all installed apps
+        for app_config in apps.get_app_configs():
+            # Check if tasks.py exists in the app's directory
+            tasks_path = os.path.join(app_config.path, "tasks.py")
+
+            if os.path.exists(tasks_path):
+                # Dynamically import the tasks module from the app
+                module_name = f"{app_config.name}.tasks"
+                tasks_module = importlib.import_module(module_name)
+
+                # Check if the `periodic` variable exists in the module
+                if hasattr(tasks_module, "periodic"):
+                    periodic = getattr(tasks_module, "periodic")
+                    periodic_specs += periodic
+
+        IntervalSchedule.objects.all().delete()
+        CrontabSchedule.objects.all().delete()
+        PeriodicTask.objects.all().delete()
+
+        for periodic_task in periodic_specs:
+            print(f'Setting up {periodic_task["task"].name}')
+
+            cron, _ = CrontabSchedule.objects.get_or_create(**periodic_task["cron"])
+
+            PeriodicTask.objects.create(
+                name=periodic_task["name"],
+                task=periodic_task["task"].name,
+                crontab=cron,
+                enabled=periodic_task["enabled"],
+            )
