eb70888feaa4ec09251f27e567214225057758e2
Harry Khanna <harry@khanna.law>
2023-10-26 18:06:36 -0400

    alert django-web-component

--- core/components.py
+++ core/components.py
@@ -27,3 +27,58 @@ def input(context):
         </div>
         """
     ).render(context)
+
+
+@component.register("alert")
+class Alert(component.Component):
+    """Usage:
+    {% #alert level="info" dismissable=True headline="This is a headline" %}
+    OR
+    {% alert level="info" dismissable=True headline="This is a headline" %}This is an optional body.{% endalert %}
+    """
+
+    template_name = "core/components/alert.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        variant = self.attributes.pop("variant", "info")
+        icons = {
+            "success": "check-circle",
+            "warning": "exclamation-circle",
+            "info": "information-circle",
+            "error": "x-circle",
+        }
+        colors = {
+            "success": {
+                "bg": "bg-green-100",
+                "icon": "text-green-500",
+                "headline": "text-green-800",
+                "body": "text-green-700",
+                "button": "bg-green-100 text-green-700 hover:bg-green-100 focus:ring-green-600",
+            },
+            "warning": {
+                "bg": "bg-yellow-50",
+                "icon": "text-yellow-400",
+                "headline": "text-yellow-800",
+                "body": "text-yellow-600",
+                "button": "bg-yellow-50 text-yellow-700 hover:bg-yellow-50 focus:ring-yellow-600",
+            },
+            "info": {
+                "bg": "bg-blue-100",
+                "icon": "text-blue-500",
+                "headline": "text-blue-700",
+                "body": "text-blue-600",
+                "button": "bg-blue-100 text-blue-700 hover:bg-blue-100 focus:ring-blue-600",
+            },
+            "error": {
+                "bg": "bg-red-50",
+                "icon": "text-red-400",
+                "headline": "text-red-800",
+                "body": "text-red-700",
+                "button": "bg-red-50 text-red-800 hover:bg-red-50 focus:ring-red-700",
+            },
+        }
+
+        return {
+            "colors": colors[variant],
+            "icon": icons[variant],
+        }
