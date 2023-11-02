56fed335818613dd715561614c08b8a0d7b251d2
Harry Khanna <harry@khanna.law>
2023-11-02 13:49:42 -0400

    Missing rename from base to core

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,4 +1,4 @@
 import pytest
 
 # Needed for proper introspection of these assertions when they fail.
-pytest.register_assert_rewrite("base.tests.assertions")
+pytest.register_assert_rewrite("core.tests.assertions")
