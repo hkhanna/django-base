0acffd63d1d991551fde5ef7dc3f4fc2e3cd9254
Harry Khanna <harry@khanna.law>
2023-11-21 18:12:29 -0500

    Remove django_react_templatetags

--- config/settings/common.py
+++ config/settings/common.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
     # core should come before staticfiles because we override collectstatic
     "django.contrib.staticfiles",
-    "django_react_templatetags",
     "django.contrib.admin",
     "allauth",
     "allauth.account",
@@ -90,7 +89,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django_react_templatetags.context_processors.react_context_processor",
             ],
             "builtins": [
                 "django_web_components.templatetags.components",
