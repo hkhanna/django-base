6db084d9a3cbfb2b93e171f145ed9692cbf44505
Harry Khanna <harry@khanna.law>
2023-11-21 16:29:51 -0500

    Refactor django_react_templatetags usage

--- frontend/src/js/react.jsx
+++ frontend/src/js/react.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 
-window.ReactDOMClient = ReactDOMClient;
-window.React = React;
-
-export const Test = ({hi}) => (
-  <div>
-    <h1>Hello, {hi} world!</h1>
+export const Example = ({ prop1 }) => (
+  <div className="text-gray-700 text-sm">
+    This is a React component. Prop: {prop1}
   </div>
 );
 
-window.Test = Test
+const Components = {
+  Example,
+};
+
+const component_json = JSON.parse(
+  document.getElementById("django_react_templatetags_components").textContent
+);
+component_json.forEach((component) => {
+  const container = document.getElementById(component.identifier);
+  const root = ReactDOMClient.createRoot(container);
+  root.render(
+    React.createElement(Components[component.name], component.json_obj)
+  );
+});
