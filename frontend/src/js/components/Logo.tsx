46c16b52030c8ab34d6091be192cf5e692d36162
Harry Khanna <harry@khanna.law>
2024-01-15 16:30:35 -0500

    Refactor Logo component

---
+++ frontend/src/js/components/Logo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text } from "@/components/catalyst/text";
+
+export default function Logo() {
+  return (
+    <div className="flex flex-col items-center text-zinc-900 dark:text-white">
+      <svg
+        className="mx-auto h-10"
+        fill="none"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+        />
+      </svg>
+      <Text>django-base</Text>
+    </div>
+  );
+}
--- frontend/src/js/pages/core/PasswordReset.tsx
+++ frontend/src/js/pages/core/PasswordReset.tsx
@@ -11,6 +11,7 @@ import { useForm } from "@inertiajs/react";
 import { Text, TextLink } from "@/components/catalyst/text";
 import { Button } from "@/components/catalyst/button";
 import Layout from "@/components/Layout";
+import Logo from "@/components/Logo";
 
 export default function PasswordReset() {
   const { data, setData, post, processing, errors } = useForm({
@@ -33,24 +34,7 @@ export default function PasswordReset() {
       <Head title="Password Reset" />
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="flex flex-col items-center text-zinc-900 dark:text-white">
-            <svg
-              className="mx-auto h-10"
-              fill="none"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
-              />
-            </svg>
-            <Text>django-base</Text>
-          </div>
+          <Logo />
           <h2 className="my-4 text-center text-3xl font-bold leading-9 tracking-tight text-zinc-800 dark:text-zinc-300">
             Password Reset
           </h2>
