c1825685a647677a4d6f9b3412846f61f8131f33
Harry Khanna <harry@khanna.law>
2024-02-27 17:52:16 -0500

    Remove unused core utils

--- core/utils.py
+++ core/utils.py
@@ -2,7 +2,6 @@ import json
 from importlib import import_module
 from typing import Callable, Optional, TYPE_CHECKING
 
-from django.conf import settings
 from django.contrib.messages import get_messages
 
 from inertia import render
@@ -40,60 +39,6 @@ def inertia_render(request, component, props={}, template_data={}):
         props={"messages": messages} | props,
         template_data=template_data,
     )
-
-
-# This is used to test formset submissions and to create synthetic formset data.
-# See https://stackoverflow.com/a/64354805
-def formset_post_data(formset, update=[]):
-    """Return a dictionary payload of a formset that can be posted.
-    `update` merges provided keys. Keys that are left out as left as-is."""
-    prefix_template = formset.empty_form.prefix  # default is 'form-__prefix__'
-
-    # extract formset data
-    management_form_data = formset.management_form.initial
-
-    # form.fields is a dict of the form's fields, including
-    # fields added by (model)formset.add_fields such as -DELETE and -id.
-    form_data_list = []
-    for form in formset:
-        form_dict = {}
-        for bound_field in form:
-            name = bound_field.name
-            value = bound_field.value() or ""
-            form_dict[name] = str(value)
-        form_data_list.append(form_dict)
-
-    # initialize the post data dict
-    post_data = {}
-
-    # add properly prefixed management form fields
-    for key, value in management_form_data.items():
-        prefix = prefix_template.replace("__prefix__", "")
-        post_data[prefix + key] = str(value)
-
-    # If we added forms, update the management form.
-    # N.B. We don't deal with deletion here.
-    if update:
-        added = len(update) - len(form_data_list)
-        if added > 0:
-            post_data[prefix + "TOTAL_FORMS"] = len(update)
-            empty = {bound_field.name: "" for bound_field in formset.empty_form}
-            form_data_list.extend([empty] * added)
-
-    # add properly prefixed data form fields
-    for index, form_data in enumerate(form_data_list):
-        for key, value in form_data.items():
-            prefix = prefix_template.replace("__prefix__", f"{index}-")
-            try:
-                post_data[prefix + key] = update[index][key]
-            except (IndexError, KeyError):
-                post_data[prefix + key] = str(value)
-
-    return post_data
-
-
-def form_post_data(form, update={}):
-    return {field.name: field.value() for field in form} | update
 
 
 def get_email_display_name(
