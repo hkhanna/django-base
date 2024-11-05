46c7b021f18818d1691615b2d7d5a02ed8753464
Harry Khanna <harry@khanna.law>
2024-11-04 20:06:38 -0500

    Remove catalyst sidebar from layout page

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,33 +2,20 @@ import React from "react";
 import { usePage } from "@inertiajs/react";
 import { useEffect } from "react";
 import { User } from "@/types/User";
-import { Navbar } from "@/components/catalyst/navbar";
-import { Avatar } from "@/components/catalyst/avatar";
-import {
-  Sidebar as CatalystSidebar,
-  SidebarBody,
-  SidebarItem,
-  SidebarLabel,
-  SidebarSection,
-  SidebarFooter,
-} from "@/components/catalyst/sidebar";
-import { SidebarLayout } from "@/components/catalyst/sidebar-layout";
-import {
-  Dropdown,
-  DropdownButton,
-  DropdownDivider,
-  DropdownItem,
-  DropdownLabel,
-  DropdownMenu,
-} from "@/components/catalyst/dropdown";
-import {
-  ChevronUpIcon,
-  UserGroupIcon,
-  Cog8ToothIcon,
-  ArrowRightStartOnRectangleIcon,
-} from "@heroicons/react/20/solid";
-import { Logo } from "./Logo";
 
+/**
+ * This is the layout for application pages. It can include things like a sidebar or navbar.
+ */
+export function ApplicationLayout({ children }: { children: React.ReactNode }) {
+  const page = usePage<{ user: User }>();
+
+  return <Layout>{children}</Layout>;
+}
+
+/**
+ * This is the base layout. It's used for all pages, including pages like login and signup that are not
+ * "application" pages. The latter would have things like sidebars etc.
+ */
 export function Layout({ children }: { children: React.ReactNode }) {
   const page = usePage<{
     messages: {
@@ -53,84 +40,5 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <main>{children}</main>
       {/* <Toaster richColors /> FIXME */}
     </>
-  );
-}
-
-export function ApplicationLayout({ children }: { children: React.ReactNode }) {
-  const page = usePage<{ user: User }>();
-
-  return (
-    <Layout>
-      <SidebarLayout
-        sidebar={<Sidebar user={page.props.user} />}
-        navbar={<Navbar />}
-      >
-        {children}
-      </SidebarLayout>
-    </Layout>
-  );
-}
-
-function Sidebar({ user }: { user: User }) {
-  const page = usePage();
-  const path = new URL(page.url).pathname;
-
-  const firstInitial = user.first_name.charAt(0);
-  const lastInitial = user.last_name.charAt(0);
-  const initials = `${firstInitial}${lastInitial}`;
-
-  return (
-    <CatalystSidebar>
-      <SidebarBody>
-        <div className="mb-2 flex">
-          <Logo />
-        </div>
-        <SidebarSection>
-          <SidebarItem
-            href="/user/settings/profile/"
-            current={path.startsWith("/user/settings/profile/")}
-          >
-            <UserGroupIcon />
-            <SidebarLabel>Profile (Replace Me)</SidebarLabel>
-          </SidebarItem>
-          <SidebarItem
-            href="/user/settings/password/"
-            current={path.startsWith("/user/settings/password/")}
-          >
-            <Cog8ToothIcon />
-            <SidebarLabel>Password (Replace Me)</SidebarLabel>
-          </SidebarItem>
-        </SidebarSection>
-      </SidebarBody>
-      <SidebarFooter>
-        <Dropdown>
-          <DropdownButton as={SidebarItem}>
-            <span className="flex min-w-0 items-center gap-3">
-              <Avatar initials={initials} className="size-10" square alt="" />
-              <span className="min-w-0">
-                <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
-                  {user.display_name}
-                </span>
-                <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
-                  {user.email}
-                </span>
-              </span>
-            </span>
-            <ChevronUpIcon />
-          </DropdownButton>
-          <DropdownMenu className="min-w-64" anchor="top start">
-            <DropdownItem href="/user/settings/profile">
-              <Cog8ToothIcon />
-              <DropdownLabel>Settings</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/user/logout">
-              <ArrowRightStartOnRectangleIcon />
-              <DropdownLabel>Sign out</DropdownLabel>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </SidebarFooter>
-    </CatalystSidebar>
   );
 }
