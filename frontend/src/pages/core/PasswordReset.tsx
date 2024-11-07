0a10a6a93e8a399a26f09ffc794d5b547d8abe7a
Harry Khanna <harry@khanna.law>
2024-11-07 14:35:57 -0500

    Install shadcn-ui label and input components

--- frontend/src/pages/core/PasswordReset.tsx
+++ frontend/src/pages/core/PasswordReset.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Head } from "@/components/Head";
-import {
-  Field,
-  Label,
-  ErrorMessage,
-  Fieldset,
-} from "@/components/catalyst/fieldset";
-import { Input } from "@/components/catalyst/input";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useForm } from "@inertiajs/react";
 import { Link } from "@/components/Link";
-import { Button } from "@/components/catalyst/button";
 import { Layout } from "@/components/Layout";
 import { Logo } from "@/components/Logo";
+import { Button } from "@/components/ui/button";
 
 export default function PasswordReset() {
   const { data, setData, post, processing, errors } = useForm({
@@ -50,48 +45,48 @@ export default function PasswordReset() {
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white dark:bg-zinc-900 dark:border dark:border-white/10 px-6 py-10 shadow sm:rounded-lg sm:px-12">
             {success ? (
-              <p className="font-medium">
+              <p className="leading-7 [&:not(:first-child)]:mt-6">
                 If an account exists with that email address, we sent you an
                 email with a link to reset your password.
               </p>
             ) : (
-              <form onSubmit={handleSubmit}>
-                <Fieldset
-                  aria-label="Password reset form"
-                  className="space-y-5"
-                >
-                  <Field>
-                    <Label>Email address</Label>
-                    <Input
-                      name="email"
-                      type="email"
-                      value={data.email}
-                      onChange={(e) => setData("email", e.target.value)}
-                      required
-                      invalid={"email" in errors}
-                    />
-                    {errors.email && (
-                      <ErrorMessage>{errors.email}</ErrorMessage>
-                    )}
-                  </Field>
-                  {"__all__" in errors && (
-                    <Field>
-                      <ErrorMessage>
-                        {errors.__all__ as React.ReactNode}
-                      </ErrorMessage>
-                    </Field>
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="space-y-2">
+                  <Label
+                    htmlFor="email"
+                    className={errors.email && "text-red-500"}
+                  >
+                    Email address
+                  </Label>
+                  <Input
+                    name="email"
+                    type="email"
+                    value={data.email}
+                    onChange={(e) => setData("email", e.target.value)}
+                    className={
+                      errors.email && "border-red-500 ring-2 ring-red-500"
+                    }
+                    required
+                  />
+                  {errors.email && (
+                    <p className="text-sm text-red-500">{errors.email}</p>
                   )}
+                </div>
+                {"__all__" in errors && (
+                  <p className="text-sm text-red-500">
+                    {errors.__all__ as React.ReactNode}
+                  </p>
+                )}
 
-                  <div>
-                    <Button
-                      type="submit"
-                      className="flex w-full justify-center"
-                      disabled={processing}
-                    >
-                      Send password reset email
-                    </Button>
-                  </div>
-                </Fieldset>
+                <div>
+                  <Button
+                    type="submit"
+                    className="flex w-full justify-center"
+                    disabled={processing}
+                  >
+                    Send password reset email
+                  </Button>
+                </div>
               </form>
             )}
           </div>
