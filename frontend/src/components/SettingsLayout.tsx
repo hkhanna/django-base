d7e98c93ccc9134531d1325574ed5c731706d330
Harry Khanna <harry@khanna.law>
2025-10-12 14:28:57 -0400

    Add playwright mcp and fix minor styling issue

--- frontend/src/components/SettingsLayout.tsx
+++ frontend/src/components/SettingsLayout.tsx
@@ -30,7 +30,7 @@ export function SettingsLayout({
         </div>
         <Separator />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="-mx-4 lg:mr-8 lg:w-1/5">
             <SettingsSidebarNav active={active} />
           </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
